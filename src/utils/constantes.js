b9008971454e6a64b082eccf2104311e3bac8e91
imlavigne <matias.lavigne@gmail.com>
2024-01-30 20:29:56 -0300

    integradorv56

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://integrador-backend-three.vercel.app';
+// export const BASE_URL = 'https://integrador-backend-three.vercel.app';
+export const BASE_URL = 'https://integrador-nucba-backend.vercel.app';
 // export const BASE_URL = 'https://nucbaz-api.vercel.app';
